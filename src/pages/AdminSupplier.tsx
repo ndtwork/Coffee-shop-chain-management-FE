853b447c9331ccac20c4a6871f65d3405af4ed4b
tú <nguyenductu.work@gmail.com>
2024-12-11 14:02:38 +0700

    Update AdminSupplier.tsx

---
+++ src/pages/AdminSupplier.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const AdminSupplier: React.FC = () => {
+  return (
+    <div className="admin-home-container">
+      <h2>Welcome to Admin Supplier</h2>
+      <p>This is the Admin Supplier page.</p>
+    </div>
+  );
+};
+
+export default AdminSupplier;
